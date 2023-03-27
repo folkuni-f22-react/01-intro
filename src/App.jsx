cf505b92e0079ab5053cd0a35f3d9c69ef46f657
David Andersson <david.andersson@zocom.se>
2023-03-27 14:00:31 +0200

    Style

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,44 @@ function App() {
 		<div className="App">
 			Hello world!
 
+			<ToggleColor />
+
 			<section>
 				<h3> React </h3>
 				<p> Värdet är: {value}. </p>
 				<button onClick={() => { setValue(value + 1) }}> Öka värde </button>
 			</section>
 
+			<Parent />
 		</div>
 	)
 }
+
+const ToggleColor = () => {
+	const [color, setColor] = useState('light')
+
+	return (
+		<section className={color}>
+		Färgad komponent
+		<button onClick={() => setColor('dark')}> Byt till mörk färg </button>
+		</section>
+	)
+}
+
+// Presentational component: bara presenterar data
+const Parent = () => (
+	<div> Jag är parent till:
+		<Child /> <Child />
+	</div>
+)
+const Child = () => (
+	<>
+		<p> Jag är ett barn </p>
+		<p> Ett busigt barn </p>
+	</>
+)
+
+let exempel = Parent()
+console.log('Parent component:', exempel)
 
 export default App
